4e4ace3c248a50641d8daf1be24669bd1a5448cf
Fabien Zapata <fabien.zapata2@gmail.com>
2023-07-26 17:59:50 +0300

    fixed last task because of misunderstanding

--- text.php
+++ text.php
@@ -1,5 +1,10 @@
 <?php
 
+// useful for var_dump() with long strings for testing code
+ini_set("xdebug.var_display_max_children", '-1');
+ini_set("xdebug.var_display_max_data", '-1');
+ini_set("xdebug.var_display_max_depth", '-1');
+
 $text = <<<TXT
 <p class="big">
 	Год основания:<b>1589 г.</b> Волгоград отмечает день города в <b>2-е воскресенье сентября</b>. <br>В <b>2023 году</b> эта дата - <b>10 сентября</b>.
@@ -13,8 +18,47 @@ $text = <<<TXT
 </p>
 TXT;
 
-// assuming that words are seperated by space
-$short_text = implode(' ', array_slice(explode(' ', $text), 0, 29))."...";
+// we transform our string into an array of characters, we save cyrillics characters form thanks to preg_split() and some parameters
+$all_characters = preg_split('//u', $text, -1, PREG_SPLIT_NO_EMPTY);
+
+$is_inside_tag = false;
+$is_previous_char_end_tag = false;
+$real_words_counter = 0; // "real" words are not html tags
+
+foreach ($all_characters as $key => $character) {
+
+    if ($is_inside_tag == false) {
+
+        if ($character == '<') { // we are inside two brackets of a tag
+            $is_inside_tag = true;
+        }
+        elseif ($real_words_counter >= 29) {
+            // remove all next characters of "real" words (not html tags)
+            unset($all_characters[$key]);
+        }
+        elseif ($is_previous_char_end_tag == false && ($character == ' ' || $character == '.' || $character == '-' || $character == ':')) {
+            $real_words_counter += 1;
+
+            if ($real_words_counter == 29) {
+                $key_end_last_word = $key;
+            }
+        }
+
+        $is_previous_char_end_tag = false;
+    }
+    elseif ($is_inside_tag == true && $character == '>') {
+        $is_inside_tag = false;
+        $is_previous_char_end_tag = true;
+    }
+}
+
+// we add the "..." after the 29th word
+array_splice($all_characters, $key_end_last_word, 0, "...");
+
+// we transform our array of characters back into a string
+$short_text = implode($all_characters);
+
+//var_dump($short_text);
 
 ?>
 
